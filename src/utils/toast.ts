2b3573be0830f82d1965aa04fb3342323d6c8281
Darren-supreme <vincent_huanghd@163.com>
2021-10-30 22:12:02 +0800

    feat: 新增toast工具

--- src/utils/toast.ts
+++ src/utils/toast.ts
@@ -4,6 +4,24 @@ interface ToastPorps {
   title: string;
   icon?: 'none' | 'success';
   duration: number;
+  success?: () => Function;
+  fail?: () => Function;
+  complete?: () => Function;
+}
+
+interface LoadingProps {
+  title: string;
+  mask?: boolean;
+}
+import Taro from '@tarojs/taro';
+
+interface ToastPorps {
+  title: string;
+  icon?: 'none' | 'success';
+  duration: number;
+  success?: () => Function;
+  fail?: () => Function;
+  complete?: () => Function;
 }
 
 interface LoadingProps {
@@ -11,27 +29,52 @@ interface LoadingProps {
   mask?: boolean;
 }
 export default class Modal {
-  static toast(config: ToastPorps | string = { title: '', duration: 2000, icon: 'none' }): void {
+  static toast(config: ToastPorps | string = {
+    title: '', duration: 2000, icon: 'none',
+  }): Promise<any> {
     let duration = 2000;
     if (typeof config === 'string') {
-      Taro.showToast({
+      return Taro.showToast({
         title: config,
         icon: 'none',
         duration,
       });
-    } else {
-      duration = config.duration || duration;
-      Taro.showToast({
-        title: config.title,
-        icon: config.icon,
-        duration,
-      });
     }
+    duration = config.duration || duration;
+    return Taro.showToast({
+      ...config,
+      title: config.title,
+      icon: config.icon,
+      duration,
+    });
   }
 
   static loading(config: LoadingProps = { title: '加载中...', mask: false }): void{
     Taro.showLoading({
       ...config,
+    });
+  }
+
+  static hideLoading(): void{
+    Taro.hideLoading();
+  }
+
+  static showModal(config: any):void{
+    const successCallback = config.success;
+    const cancelCallback = config.concel;
+    Taro.showModal({
+      title: config.title || '提示',
+      content: config.content || '',
+      success: (res) => {
+        if (res.confirm) {
+          if (successCallback && typeof successCallback === 'function') {
+            successCallback();
+          }
+        } else if (cancelCallback && typeof cancelCallback === 'function') {
+          cancelCallback();
+        }
+      },
+      fail: () => cancelCallback && cancelCallback(),
     });
   }
 }
