95686ecfa5204ed3f19d9cf9ded5d98e8acb11b6
Darren-supreme <vincent_huanghd@163.com>
2021-10-30 22:17:41 +0800

    feat: 新增版本更新提示

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ import { Component } from 'react';
 import { Provider } from 'react-redux';
 import Taro from '@tarojs/taro';
 import configStore from '@/redux/store';
-
+import Modal from './utils/toast';
 import './app.less';
 
 const store = configStore();
@@ -49,7 +49,18 @@ class App extends Component {
         })
         updateManager.onUpdateFailed(() => {
           // 新版本下载失败
+          Modal.showModal({
+            title: '已经有新版本了哟~',
+            content: '新版本已经上线啦，请您删除当前小程序，重新搜索打开哟~',
+            showCancel: false,
+          });
         })
+      } else {
+        Modal.showModal({
+          title: '提示',
+          content: '当前微信版本过低，请升级到最新微信版本后重新打开小程序~',
+          showCancel: false,
+        });
       }
     }
   // 在 App 类中的 render() 函数没有实际作用
